7e7c26e4658a9a7d238e0b8d8f9c8e2c2cbc2da2
Will Taylor <william.h.taylor@stfc.ac.uk>
2024-08-13 16:14:04 +0100

    A little tidying/documenting

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ thread_patt = r"\[(\d+|\.NET TP Worker)\]"
 level_patt = "(?P<level>[A-Z]+)"
 logging_class_patt = r"([a-zA-Z0-9]+\.)+(?P<logging_class>[a-zA-Z0-9]+)"
 pattern = f"{date_patt} {time_patt} {thread_patt} {level_patt} {logging_class_patt} - (?P<message>.+)"
-def strip_crap(line):
+def tidy_log_line(line):
     m = re.match(pattern, line)
     if not m:
         return line
@@ -46,6 +46,8 @@ class FollowedLog(Static):
 
     @work(thread=True)
     def setup(self) -> None:
+        """ Load the log file, skipping to the last CATCHUP_LINES """
+
         line_end_positions = []
         with open(self.filename, "rb") as f:
             for line in f:
@@ -62,22 +64,24 @@ class FollowedLog(Static):
 
     @work(thread=True)
     def poll_lines(self) -> None:
+        # This method and `setup` run concurrently, so the file may not be open yet
         if not hasattr(self, "tail") or not self.tail:
             return
 
         if (new_lines := self.get_new_lines()):
             self.post_message(self.LogUpdated(new_lines))
 
-    def on_followed_log_log_updated(self, ev: LogUpdated) -> None:
-        self.lines = self.lines + ev.new_log_lines
-
     def get_new_lines(self) -> [str]:
         new_lines = []
         while (l := self.tail.readline()):
-            new_lines.append(strip_crap(l))
+            new_lines.append(tidy_log_line(l))
         return new_lines
 
+    def on_followed_log_log_updated(self, ev: LogUpdated) -> None:
+        self.lines = self.lines + ev.new_log_lines
+
     def watch_lines(self, lines: list[str]) ->  None:
+        """ When lines is updated, write the new ones to the log """
         log = self.query_one(RichLog)
         if self.lines and self.lines_read < len(self.lines):
             for i in range(self.lines_read + 1, len(self.lines) + 1):
@@ -114,6 +118,7 @@ class LogFollowApp(App):
         yield Footer()
 
     def on_key(self, event: events.Key) -> None:
+        """ 1-9 keys can be used to make a single log full screen, 0 resplits into columns """
         if event.key.isdigit():
             i = int(event.key)
 
